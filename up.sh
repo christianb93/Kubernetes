1d20cfb475ee47715d5c638141f6e34c36956593
christianb93 <christian_bohr@web.de>
2019-03-16 22:29:53 +0100

    Added filter

--- up.sh
+++ up.sh
@@ -10,7 +10,7 @@ python3 create_nodes.py
 #
 # Open security group for incoming SSH connections from my IP
 #
-SEC_GROUP_ID=$(aws ec2 describe-instances --output text --query Reservations[0].Instances[0].SecurityGroups[0].GroupId)
+SEC_GROUP_ID=$(aws ec2 describe-instances --output text --filters Name=tag-key,Values=kubernetes.io/cluster/myCluster --query Reservations[0].Instances[0].SecurityGroups[0].GroupId)
 myIP=$(wget -q -O- https://ipecho.net/plain)
 aws ec2 authorize-security-group-ingress --group-id $SEC_GROUP_ID --port 22 --protocol tcp --cidr "$myIP/32"
 
